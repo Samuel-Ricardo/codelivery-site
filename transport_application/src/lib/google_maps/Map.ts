8dce531d4bcd73cf5b67a475f76f9944f9d16de2
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-04 21:59:11 -0300

    :sparkles: create: fun - addRoute (Map)

--- transport_application/src/lib/google_maps/Map.ts
+++ transport_application/src/lib/google_maps/Map.ts
@@ -1,5 +1,6 @@
 import { google } from "google-maps";
 import { Route } from "./Route";
+import { RouteAlredyExistsError } from "@/errors";
 
 export class Map {
   public map: google.maps.Map;
@@ -17,5 +18,25 @@ export class Map {
     const route = this.routes[id];
     route.delete();
     delete this.routes[id];
+  }
+
+  addRoute(
+    id: string,
+    routeOptions: {
+      currentMarkerOptions: google.maps.ReadonlyMarkerOptions;
+      endMarkerOptions: google.maps.ReadonlyMarkerOptions;
+    }
+  ) {
+
+    if (id in this.routes) throw new RouteAlredyExistsError();
+
+    const {currentMarkerOptions, endMarkerOptions} = routeOptions;
+
+    this.routes[id] = new Route({
+      currentMakerOptions: {...currentMarkerOptions, map: this.map},
+      endMarkerOptions: {...endMarkerOptions, map: this.map}
+    })
+
+    this.fitBounds();
   }
 }
