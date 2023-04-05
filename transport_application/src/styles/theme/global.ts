945fafcc429469d852a0c64168ad8aad392a488c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-05 08:49:10 -0300

    :lipstick: create: theme - global

---
+++ transport_application/src/styles/theme/global.ts
@@ -0,0 +1,14 @@
+import { PaletteOptions, createTheme } from "@mui/material";
+
+const palette: PaletteOptions = {
+  mode: "dark",
+  primary: {
+    main: "#FFCD00",
+    contrastText: "#242526"
+  },
+  background: { default: "#242526" }
+}
+
+const theme = createTheme({palette})
+
+export default theme;
