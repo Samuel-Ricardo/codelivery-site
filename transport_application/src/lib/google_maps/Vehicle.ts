07d8d5a4abb9058e525478ee218ad03dd7365f0a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-04 22:08:57 -0300

    :sparkles: create: fun - makeMakerIcon (Vehicle)

--- transport_application/src/lib/google_maps/Vehicle.ts
+++ transport_application/src/lib/google_maps/Vehicle.ts
@@ -8,3 +8,13 @@ export const makeCarIcon = (color: string) => ({
   anchor: new google.maps.Point(26, 20),
 });
 
+export const makeMarkerIcon = (color: string) => ({
+  path:
+    "M66.9,41.8c0-11.3-9.1-20.4-20.4-20.4c-11.3,0-20.4,9.1-20.4,20.4c0,11.3,20.4,32.4,20.4,32.4S66.9,53.1,66.9,41.8z    M37,41.4c0-5.2,4.3-9.5,9.5-9.5c5.2,0,9.5,4.2,9.5,9.5c0,5.2-4.2,9.5-9.5,9.5C41.3,50.9,37,46.6,37,41.4z",
+  strokeColor: color,
+  fillColor: color,
+  strokeOpacity: 1,
+  strokeWeight: 1,
+  fillOpacity: 1,
+  anchor: new google.maps.Point(46, 70),
+});
