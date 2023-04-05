ff2883ce592acaf3312715df1f511b478461842c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-04 21:23:31 -0300

    :sparkles: create: constructor - Route

--- transport_application/src/lib/google_maps/Route.ts
+++ transport_application/src/lib/google_maps/Route.ts
@@ -5,4 +5,26 @@ export class Route {
   public endMaker: google.maps.Marker;
   private directionRenderer: google.maps.DirectionsRenderer;
 
+  constructor (options: {
+    currentMakerOptions: google.maps.ReadonlyMarkerOptions;
+    endMarkerOptions: google.maps.ReadonlyMarkerOptions;
+  }){
+    
+    const {currentMakerOptions,endMarkerOptions} = options;
+
+    this.currentMaker = new google.maps.Marker(currentMakerOptions);
+    this.endMaker = new google.maps.Marker(endMarkerOptions) 
+
+    const strokeColor = (this.currentMaker.getIcon() as google.maps.ReadonlySymbol).strokeColor;
+
+    this.directionRenderer = new google.maps.DirectionsRenderer({
+      suppressMarkers: true,
+      polylineOptions: {
+        strokeColor,
+        strokeOpacity: 0.5,
+        strokeWeight: 5,
+      }
+    })
+    this.directionRenderer.setMap(this.currentMaker.getMap() as google.maps.Map);
+  }
 }
