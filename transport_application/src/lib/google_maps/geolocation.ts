d6b5829e2557a96ea6922e9dfc74390ccbb18482
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-04 21:20:58 -0300

    :sparkles: implements: google maps - geolocation

---
+++ transport_application/src/lib/google_maps/geolocation.ts
@@ -0,0 +1,22 @@
+import { Position } from "@/@types";
+
+export function getCurrentPosition (options?:PositionOptions): Promise<Position> {
+  
+  return new Promise((resolve, reject) => {
+
+    navigator.geolocation.getCurrentPosition(
+      
+      position => resolve ({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      }),
+
+      error => reject(error),
+
+      options
+    
+    );
+
+  })
+
+}
