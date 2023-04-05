67a48f3030acfd1277cd4e31df1b760358ea4d07
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-05 09:58:04 -0300

    :lipstick: create: style - mapping_style

---
+++ transport_application/src/styles/mapping_style.ts
@@ -0,0 +1,24 @@
+import { makeStyles } from "@mui/material"
+
+export const  mapping_style = makeStyles({
+  
+  root: {
+    width: "100%",
+    height: "100%"
+  },
+
+  form: {
+    margin: "16px",
+  },
+
+  btnSubmitWrapper: {
+    textAlign: "center",
+    marginTop: "8px",
+  },
+
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+
+});
