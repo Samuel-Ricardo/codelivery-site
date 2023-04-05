c058c8f9f6bc018df28979126b7575f2c5e43c89
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-04 21:41:35 -0300

    :sparkles: create: constructor  (Map)

--- transport_application/src/lib/google_maps/Map.ts
+++ transport_application/src/lib/google_maps/Map.ts
@@ -4,5 +4,7 @@ export class Map {
   public map: google.maps.Map;
   private routes: {[id: string]: Route} = {};
 
-  
+ cosntructor(element: Element, options: google.maps.MapOptions){
+    this.map = new google.maps.Map(element, options);
+  } 
 }
