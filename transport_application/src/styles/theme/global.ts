81dd7c5ce952091fe72afd6b774f01557b3088df
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-06 10:08:19 -0300

    :lipstick: create: route_colors (themes/global)

--- transport_application/src/styles/theme/global.ts
+++ transport_application/src/styles/theme/global.ts
@@ -12,3 +12,16 @@ const palette: PaletteOptions = {
 const theme = createTheme({palette})
 
 export default theme;
+
+export const route_colors = [
+  "#b71c1c",
+  "#4a148c",
+  "#2e7d32",
+  "#e65100",
+  "#2962ff",
+  "#c2185b",
+  "#FFCD00",
+  "#3e2723",
+  "#03a9f4",
+  "#827717",
+]
