0d60f6347843745a25d41c258bb2ea653afb3d6b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-09 13:20:11 -0300

    :sparkles: update: file - connections (src/api)

--- transport_application/src/api/connection.ts
+++ transport_application/src/api/connection.ts
@@ -1,15 +1,14 @@
-import { GOOGLE_API_KEY } from "@/config";
-import { useRef } from "react";
+import { API_URL } from "@/config";
 import { Socket, connect } from "socket.io-client";
 
-const socket_client = useRef<Socket>();
+var socket_client:Socket;
 
-export function getConnection() {
+export const getConnection = () => {
 
-  if (socket_client.current?.connected) return socket_client 
+  if (socket_client?.connected) return socket_client 
+
+  socket_client = connect(API_URL()) 
+  socket_client.on("connect", () => console.log("Connected! :D"))
   
-  socket_client.current = connect(GOOGLE_API_KEY()) 
-  socket_client.current.on("connect", () => console.log("Connected! :D"))
-
-  return socket_client;
+  return socket_client; 
 }
