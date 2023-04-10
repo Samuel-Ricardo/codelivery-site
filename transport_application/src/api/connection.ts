791b4391297d145a10d88d0756b4be46fc8688c5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-10 11:58:08 -0300

    :sparkles: update: Connections - websocket

--- transport_application/src/api/connection.ts
+++ transport_application/src/api/connection.ts
@@ -1,14 +1,31 @@
-import { API_URL } from "@/config";
+import { API_URL, EVENTS } from "@/config";
+import { Console } from "console";
 import { Socket, connect } from "socket.io-client";
 
-var socket_client:Socket;
+const socket_client:Socket = connect(API_URL());
+
+
+  socket_client.on('connect', () => console.log("Connected! :D"))
+
+  socket_client.onAnyOutgoing((event, ...args) => console.log({event, args}))
+  socket_client.onAny((event, ...args) => console.log({event, args}))
+
+  socket_client.on('disconnect', (...args) => console.log("Client disconnected", {args}))
+
 
 export const getConnection = () => {
 
   if (socket_client?.connected) return socket_client 
 
-  socket_client = connect(API_URL()) 
-  socket_client.on("connect", () => console.log("Connected! :D"))
-  
+  socket_client.connect()
+  socket_client.on("connect", (...args) =>{
+
+    console.log("Connected! 2 :D", {args})
+
+    socket_client.on('disconnect', (...args) => {
+      console.log('Client disconnected. 2', {args});
+    });
+  })
+
   return socket_client; 
 }
