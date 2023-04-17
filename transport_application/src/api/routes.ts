f656712e50af85a545409df22e57e1129a9db209
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-04-17 18:04:46 -0300

    :mute: remove: log

--- transport_application/src/api/routes.ts
+++ transport_application/src/api/routes.ts
@@ -2,5 +2,8 @@ import { GET_ALL_ROUTES } from "@/config/api/routes";
 import { IRouteResponse } from '@types' 
  
 
+console.log({PEDRO: GET_ALL_ROUTES});
+
+
 export const getALL = 
   () => fetch(GET_ALL_ROUTES).then(data => data.json()) as Promise<IRouteResponse[]>
